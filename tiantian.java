2d2fdd3a0c247a294ec73114bbb5a88b41f0a6f9
daydayup <ghostrider2014@163.com>
2016-11-10 11:12:10 +0800

    use --no-ff

--- tiantian.java
+++ tiantian.java
@@ -7,5 +7,6 @@ public static void main(String args){
 	System.out.println("tiantian xiang shang");
 	System.out.println("我们是祖国的花朵");
 	System.out.println("我们要改变世界！！！！");
+	System.out.println("use --no-ff vararity:);
 
 }
