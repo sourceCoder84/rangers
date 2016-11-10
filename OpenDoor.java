e1960b8625d8a524f482bca474579623eabee78b
daydayup <ghostrider2014@163.com>
2016-11-10 15:58:20 +0800

    change the content of the OpenDoor.java

--- OpenDoor.java
+++ OpenDoor.java
@@ -4,7 +4,7 @@ public class OpenDoor{
 
 	public static void main(String[] args){
 
-		System.out.println("tian tian xiang shang");
+		System.out.println("day day up");
 		System.out.println("edit again");
 	}
 }
