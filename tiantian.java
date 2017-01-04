d1d585e298b75cf0aa3c85c4df68357c67a63ec8
tiantianxiangshang <ghostrider2014@163.com>
2017-01-04 16:18:15 +0800

    remove tiantian.java file

--- tiantian.java
+++
@@ -1,9 +0,0 @@
-/*
-* Test to add a file to remote store
-*/
-
-public static void main(String args){
-
-	System.out.println("tiantian xiang shang");
-	System.out.println("women de zuguoshi huayuan");
-}
